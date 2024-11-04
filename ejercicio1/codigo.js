04a0884c12bd9c50a21ef768d82b4fcf0c80fa76
Alejandro Fernandez <alexxfernandez@hotmail.es>
2024-11-04 03:51:00 +0100

    Add files via upload

---
+++ ejercicio1/codigo.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const primerMensajeElement = document.getElementById('primerMensaje');
+    const mensajeElement = document.getElementById('mensaje');
+    
+    primerMensajeElement.textContent = "Esta pagina contiene el primer script";
+    mensajeElement.textContent = "Soy el primer script";
+});
